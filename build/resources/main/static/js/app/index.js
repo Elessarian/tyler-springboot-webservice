6a88610dfa8002391f96b87637066259bc507606
Tayler <wjdduq0417@naver.com>
2021-04-18 17:14:52 +0900

    Chap3. 머스테치로 화면 구성하기 ~p.1250

---
+++ build/resources/main/static/js/app/index.js
@@ -0,0 +1,33 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            console.log('== init 함수 진입 ==')
+            _this.save();
+        });
+    },
+    save : function () {
+        console.log('== save 함수 진입 ==')
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            console.log('글이 등록되었습니다.')
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error))
+        })
+    }
+};
+
+main.init();
